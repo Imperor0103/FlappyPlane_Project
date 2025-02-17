c2325f1c670b061d4f7f3f9e8ca8d7dba5ad9422
Imperor <imperor0103@gmail.com>
2025-02-18 03:55:41 +0900

    feat: UI추가(점수, 재시작) 게임 완성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,14 +12,27 @@ public class GameManager : MonoBehaviour
 
     private int currentScore = 0;
 
+    // UI매니저
+    UIManager uiManager;
+    public UIManager UIManager { get { return uiManager; } }    // UIManager 를 외부로 가져갈 수 있는 프로퍼티
+
     private void Awake()
     {
         gameManager = this; // 3.최초의 객체를 생성해주는 작업
+
+        // UIManager는 싱글턴 객체가 아니므로 FindObjectsOfType으로 찾는다
+        uiManager = FindObjectOfType<UIManager>();
     }
+    private void Start()
+    {
+        uiManager.UpdateScore(0);   // 시작할때는 0점
+    }
+
     // 게임 오버
     public void GameOver()
     {
         Debug.Log("Game Over");
+        uiManager.SetRestart(); // Restart 텍스트 띄운다
     }
     // 재시작
     public void RestartGame()
@@ -30,5 +43,6 @@ public class GameManager : MonoBehaviour
     {
         currentScore += score;
         Debug.Log("Score: " + currentScore);
+        uiManager.UpdateScore(currentScore);    // 점수 추가
     }
 }
